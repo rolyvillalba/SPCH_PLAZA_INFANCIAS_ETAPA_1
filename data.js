bbdc93171f12e991cfffd26fe9a56284bc55b9c5
rolyvillalba <rolyvillalba@gmail.com>
2021-06-12 00:26:29 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,425 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-211_pano__1---panorama",
+      "name": "211_PANO__1 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.5598462834177411,
+        "pitch": -0.06467144192771457,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1858532283646959,
+          "pitch": 0.12460519433819073,
+          "rotation": 0,
+          "target": "1-211_pano__2---panorama"
+        },
+        {
+          "yaw": -0.06344938687264623,
+          "pitch": 0.13650879490596068,
+          "rotation": 0,
+          "target": "6-211_pano__7---panorama"
+        },
+        {
+          "yaw": -0.5217445833605545,
+          "pitch": 0.09370982467962463,
+          "rotation": 0,
+          "target": "4-211_pano__5---panorama"
+        },
+        {
+          "yaw": -3.0345162317081673,
+          "pitch": 0.09000299103873743,
+          "rotation": 0,
+          "target": "5-211_pano__6---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-211_pano__2---panorama",
+      "name": "211_PANO__2 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.841687756043175,
+        "pitch": -0.004991483152430476,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5491090342317015,
+          "pitch": 0.13921230953346253,
+          "rotation": 0,
+          "target": "0-211_pano__1---panorama"
+        },
+        {
+          "yaw": -0.17179372575369634,
+          "pitch": 0.15664720546213395,
+          "rotation": 0,
+          "target": "4-211_pano__5---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-211_pano__3---panorama",
+      "name": "211_PANO__3 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.08367852475876703,
+        "pitch": 0.038605160715565745,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.957557069905878,
+          "pitch": 0.11562362995884357,
+          "rotation": 0,
+          "target": "3-211_pano__4---panorama"
+        },
+        {
+          "yaw": 1.1095995850103435,
+          "pitch": 0.174311243039174,
+          "rotation": 0,
+          "target": "5-211_pano__6---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-211_pano__4---panorama",
+      "name": "211_PANO__4 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.7435545572546509,
+        "pitch": 0.0674684402928385,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.442783602489019,
+          "pitch": 0.14301525199937792,
+          "rotation": 0,
+          "target": "7-211_pano__8---panorama"
+        },
+        {
+          "yaw": 1.3326113879868746,
+          "pitch": 0.06922427896543937,
+          "rotation": 0,
+          "target": "2-211_pano__3---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-211_pano__5---panorama",
+      "name": "211_PANO__5 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.350747292959534,
+          "pitch": 0.14777551990196613,
+          "rotation": 0,
+          "target": "1-211_pano__2---panorama"
+        },
+        {
+          "yaw": 0.31740479222960793,
+          "pitch": 0.1469479345544329,
+          "rotation": 0,
+          "target": "0-211_pano__1---panorama"
+        },
+        {
+          "yaw": -0.45004335527231376,
+          "pitch": 0.12814827035890453,
+          "rotation": 0,
+          "target": "6-211_pano__7---panorama"
+        },
+        {
+          "yaw": 2.9394753024182076,
+          "pitch": 0.08345907892639204,
+          "rotation": 0,
+          "target": "7-211_pano__8---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-211_pano__6---panorama",
+      "name": "211_PANO__6 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20681408932036582,
+          "pitch": 0.1687024573678606,
+          "rotation": 0,
+          "target": "2-211_pano__3---panorama"
+        },
+        {
+          "yaw": 1.290744136634629,
+          "pitch": 0.0983180384294915,
+          "rotation": 0,
+          "target": "0-211_pano__1---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-211_pano__7---panorama",
+      "name": "211_PANO__7 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.12081865540977432,
+        "pitch": -0.01902032972953016,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1075355544499246,
+          "pitch": 0.12031144414252459,
+          "rotation": 0,
+          "target": "0-211_pano__1---panorama"
+        },
+        {
+          "yaw": 0.7220510272454401,
+          "pitch": 0.09583643171963985,
+          "rotation": 0,
+          "target": "4-211_pano__5---panorama"
+        },
+        {
+          "yaw": -0.03609288753625606,
+          "pitch": 0.07649555485578396,
+          "rotation": 0,
+          "target": "1-211_pano__2---panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-211_pano__8---panorama",
+      "name": "211_PANO__8 - Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33933962682683116,
+          "pitch": 0.0874412024319895,
+          "rotation": 0,
+          "target": "4-211_pano__5---panorama"
+        },
+        {
+          "yaw": 0.5096020458469006,
+          "pitch": 0.11391283179351852,
+          "rotation": 0,
+          "target": "3-211_pano__4---panorama"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "PANORAMICAS PLAZA DE LAS INFANCIAS E1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
